7dfca43468c183387997a841597cc24dcb8b2dfc
selyxpay <selyxpay@gmail.com>
2026-08-11 14:14:16 -0300

    fix: remove email validation from zod to allow mart login

--- server/src/validation/auth.schema.ts
+++ server/src/validation/auth.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email("E-mail inválido."),
+  email: z.string().min(1, "Informe o usuário."),
   password: z.string().min(1, "Informe a senha."),
 });
